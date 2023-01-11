9ca4e5613bbf360d4bdf68e75a0e5cc9d4a4b601
Gabriel4g <gabrielprogramador11@gmail.com>
2023-01-10 22:41:13 -0500

    fix: small edits at project

--- js/main.js
+++ js/main.js
@@ -1,52 +1,80 @@
-const buttonSearch = document.querySelector("#btn-search");
-const inputSearch = document.querySelector("#input-user");
-const repositoriesContainer = document.querySelector("#repositories-container");
+const inputUser = document.querySelector("#input-user"),
+buttonSearch = document.querySelector("#btn-search"),
+containerCardsRepositories = document.querySelector("#repositories-container"),
+imageUser = document.querySelector("#image-user");
 
-async function getDataFromRepositories(user) {
+async function getUserRepositories(user) {
     try {
         const response = await fetch(`https://api.github.com/users/${user}/repos`);
-        const data = await response.json();
+        
+        if(response.status === 200) {
+            const data = await response.json();
+            return data;
+        }
 
-        return data;
+        else if(response.status === 404) {
+            alert("Not Found")
+        }
+
+        else {
+            alert("Server Error")
+        }
     }
 
-    catch (error) {
-        alert(error)
+    catch(error) {
+        console.log(error)
     }
 }
 
-async function renderInfoFromRepositories(user) {
-    const data = await getDataFromRepositories(user);
-    console.log(data);
+function Put_As_Son(container, child) {
+    container.appendChild(child);
+}
 
-    data.forEach((item) => {
-        const contentRepositories = document.createElement("a");
-        contentRepositories.classList.add("card");
-        repositoriesContainer.append(contentRepositories);
-        contentRepositories.setAttribute("href", item.html_url);
-        contentRepositories.setAttribute("target", "__blank");
+async function renderUserRepositories(user) {
+    const data = await getUserRepositories(user);
 
-        const nameRepositorie = document.createElement("h3");
-        contentRepositories.append(nameRepositorie);
-        nameRepositorie.textContent = item.name;
-        nameRepositorie.classList.add("style-name-repositorie");
-
-        const descriptionRepositorie = document.createElement("p");
-        contentRepositories.append(descriptionRepositorie);
-        descriptionRepositorie.classList.add("style-description-repositorie");
-        descriptionRepositorie.textContent = `${item.description || "No Description"}`;
-
-        const publicRepositorie = document.createElement("span");
-        contentRepositories.append(publicRepositorie);
-        publicRepositorie.classList.add("style-public-text");
-        publicRepositorie.textContent = item.visibility;
+    fetch(`https://api.github.com/users/${user}`)
+    .then(res => res.json())
+    .then(profileData => {
+        imageUser.src = profileData.avatar_url;
     })
+
+    for(let dataJson of data) {
+        const cardRepository = document.createElement("a");
+        cardRepository.classList.add("card");
+        Put_As_Son(containerCardsRepositories, cardRepository);
+        cardRepository.setAttribute("href", dataJson.html_url);
+
+        const nameRepository = document.createElement("h2");
+        Put_As_Son(cardRepository, nameRepository);
+        nameRepository.textContent = dataJson.name;
+        nameRepository.classList.add("style-name-repository");
+
+        const descriptionRepository = document.createElement("p");
+        Put_As_Son(cardRepository, descriptionRepository);
+        descriptionRepository.textContent = `${dataJson.description || 'No Description'}`
+        descriptionRepository.classList.add("style-description-repository");
+
+        const visibilityRepository = document.createElement("h5");
+        Put_As_Son(cardRepository, visibilityRepository);
+        visibilityRepository.textContent = dataJson.visibility;
+        visibilityRepository.classList.add("style-visibility-repository");
+
+        const watchersRepository = document.createElement("h5");
+        Put_As_Son(cardRepository, watchersRepository);
+        watchersRepository.innerHTML = `${dataJson.watchers} <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <circle cx="12" cy="12" r="2" />
+        <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+      </svg>`;
+        watchersRepository.classList.add("style-watchers-repository");
+    }
 }
 
-buttonSearch.addEventListener("click", (e) => {
-    e.preventDefault();
+buttonSearch.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    renderInfoFromRepositories(inputSearch.value);
-    inputSearch.value = "";
-    repositoriesContainer.innerHTML = "";
+    renderUserRepositories(inputUser.value);
+    inputUser.value = "";
+    containerCardsRepositories.innerHTML = "";
 })
